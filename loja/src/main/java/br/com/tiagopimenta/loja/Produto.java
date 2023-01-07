d2d88b07e98158bc3f44748834d09a533fe599f1
Tiago Pimenta <tiagopimenta@gmail.com>
2023-01-07 17:24:58 -0300

    blah

---
+++ loja/src/main/java/br/com/tiagopimenta/loja/Produto.java
@@ -0,0 +1,5 @@
+package br.com.tiagopimenta.loja;
+
+public class Produto {
+
+}
---
+++ loja/src/test/java/br/com/tiagopimenta/loja/ProdutoTest.java
@@ -0,0 +1,11 @@
+package br.com.tiagopimenta.loja;
+
+import org.junit.Test;
+
+public class ProdutoTest {
+
+	@Test
+	public void test() {
+	}
+
+}
